fb5134cd539c34ff4e0412b47d1c4e1f04a941ec
Talika Bajaj <bajajtalika2001@gmail.com>
2024-01-27 20:04:02 +0530

    Add classname to li

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function newTask () {
     let item = document.getElementById("item-list");
     let task = document.getElementById("task").value;
     let li = document.createElement("li");
-    li.appendChild(task);
+    li.className = "list";
     item.appendChild(li);
+    li.innerHTML = task;
 }
