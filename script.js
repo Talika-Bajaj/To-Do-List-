2426c7d984b9689d01f7c1fbaf0237d9dad03487
Talika Bajaj <bajajtalika2001@gmail.com>
2024-01-27 23:36:03 +0530

    Format document

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 var check = document.getElementsByClassName("check");
 var i;
-for (i =0; i<check.length; i++) {
+for (i = 0; i < check.length; i++) {
     check[i].onclick = function () {
-    let parent = this.parentElement;
-    console.log(parent);
-    // parent.style.display = "none";
+        // let parent = this.parentElement;
+        console.log("dlkcksvnkjdndnvou");
+        // parent.style.display = "none";
     }
 }
 
 
 //function to add a new task
-function newTask () {
+function newTask() {
     let item = document.getElementById("item-list");
 
     //accesing the task from input field
@@ -28,14 +28,14 @@ function newTask () {
     // button.appendChild(txt);
     li.appendChild(check);
 
-    for (let i = 0; i<check.length; i++) {
+    for (let i = 0; i < check.length; i++) {
         check[i].onclick = function () {
-            let parent = this.parentElement;
-            console.log(parent);
+            // let parent = this.parentElement;
+            console.log("kvndkjfvndjnjfvnu");
             // parent.style.display = "none";
         }
     }
-    
+
     //creating a button to check a task
     let close = document.createElement("button");
     close.className = "close";
