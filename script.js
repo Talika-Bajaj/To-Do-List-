73982b8500ee07bf49cad97c74a5cd7b67ef092f
Talika Bajaj <bajajtalika2001@gmail.com>
2024-01-27 23:24:19 +0530

    Add functionality for list items

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
+
+var check = document.getElementsByClassName("check");
+var i;
+for (i =0; i<check.length; i++) {
+    check[i].onclick = function () {
+    let parent = this.parentElement;
+    console.log(parent);
+    // parent.style.display = "none";
+    }
+}
+
+
 //function to add a new task
 function newTask () {
     let item = document.getElementById("item-list");
+
+    //accesing the task from input field
     let task = document.getElementById("task").value;
     let li = document.createElement("li");
     li.className = "list";
-    item.appendChild(li);
-    li.innerHTML = task;
+    li.innerHTML = task;    //setting input field value in li tag
+
+    //creating a button to check a task
+    let check = document.createElement("button");
+    check.className = "check";
+    check.innerHTML = "&#10004;"
+    // let txt = document.createTextNode("&#10004;");
+    // button.appendChild(txt);
+    li.appendChild(check);
+
+    for (let i = 0; i<check.length; i++) {
+        check[i].onclick = function () {
+            let parent = this.parentElement;
+            console.log(parent);
+            // parent.style.display = "none";
+        }
+    }
+    
+    //creating a button to check a task
+    let close = document.createElement("button");
+    close.className = "close";
+    close.innerHTML = "&#10006;"
+    // let txt = document.createTextNode("&#10004;");
+    // button.appendChild(txt);
+    li.appendChild(close);
+
+    item.appendChild(li);   //adding li tag to ul tag
+
+    document.getElementById("task").value = "";
 }
