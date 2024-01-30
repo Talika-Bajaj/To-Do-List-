1a0c407b813e34a1e15f283813e36fada6db58af
Talika Bajaj <bajajtalika2001@gmail.com>
2024-01-30 18:37:51 +0530

    Add event handler to the themes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//accessing side-bar
 const hamburger = document.querySelector(".hamburger");
 const sideBar = document.querySelector(".side-bar");
 const themeMenu = document.querySelector(".theme-menu");
@@ -5,15 +6,72 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     sideBar.classList.toggle("active");
     themeMenu.classList.toggle("active");
-    // navMenu.classList.toggle("active");
-  });
+});
+
+//setting themes for the document
+let colorOne = document.querySelector(".color-one");
+colorOne.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, violet, #888, indigo)";
+})
+
+let colorTwo = document.querySelector(".color-two");
+colorTwo.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, indigo, #888, dodgerblue)";
+})
+
+let colorThree = document.querySelector(".color-three");
+colorThree.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, dodgerblue, #888, lightgreen)";
+})
+
+let colorFour = document.querySelector(".color-four");
+colorFour.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, lightgreen, #888, yellowgreen)";
+})
+
+let colorFive = document.querySelector(".color-five");
+colorFive.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, yellowgreen, #888, orange)";
+})
+
+let colorSix = document.querySelector(".color-six");
+colorSix.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, orange, #888, indianred)";
+})
+
+let colorSeven = document.querySelector(".color-seven");
+colorSeven.addEventListener("click",function () {
+    document.body.style.background = "linear-gradient(0.25turn, indianred, #888, violet6)";
+})
 
 
-let indigo = document.getElementsByClassName("ind");
-indigo.onclick = function () {
-    console.log("jbjkcbskjd");
-}
+//setting theme for to-do list
+// let colorOne = document.querySelector(".color-one");
+// colorOne.addEventListener("click",function () {
+//     console.log("jbjkcbskjd");
+//     document.getElementById("task").style.borderColor = "indigo";
+//     document.getElementById("add-btn").style.borderColor = "indigo";
+//     document.getElementById("add-btn").style.backgroundColor = "#8f25dd";
+//     let odd = document.querySelectorAll('.list:nth-child(odd)');
+//     console.log(odd);
+//     for (let i = 0; i<odd.length; i++) {
+//         odd[i].style.setProperty("background-color", "indigo", "important");
+//     }
+//     let oddCheck = document.querySelectorAll(".list:nth-child(odd) .check");
+//     console.log(oddCheck);
+//     for (let j = 0; j<oddCheck.length; j++) {
+//         oddCheck[j].style.backgroundColor = "indigo";
+//     }
 
+//     let oddClose = document.querySelectorAll(".list:nth-child(odd) .close");
+//     console.log(oddClose);
+//     for (let j = 0; j<oddClose.length; j++) {
+//         oddClose[j].style.backgroundColor = "indigo";
+//     }
+// });
+
+
+//removing a task 
 var close = document.getElementsByClassName("close");
 
 close.onclick = function () {
@@ -45,20 +103,15 @@ function newTask() {
     let check = document.createElement("button");
     check.className = "check";
     check.innerHTML = "&#10004;"
-    // let txt = document.createTextNode("&#10004;");
-    // button.appendChild(txt);
     li.appendChild(check);
 
     let complete  = document.querySelector(".complete");
     let completed  = document.querySelector(".completed");
     check.onclick = function () {
         let parent = this.parentElement;
-        // console.log(parent);
-        // parent.style.display = "none";
         let fC = parent.firstElementChild;
         console.log(fC);
         fC.remove();
-        // fC.style.display = "none";
         complete.style.display = "block";
         completed.appendChild(parent);
         parent.style.textDecoration = "line-through";
@@ -81,6 +134,7 @@ function newTask() {
     
 }
 
+//closing a pop-up
 let cross = document.querySelector(".cross");
 cross.onclick = function () {
     document.querySelector(".pop-up").style.display = "none";
